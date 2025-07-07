392ba1dbb7124ac6ae1c23dfc09641d4e4cde981
arboreal <samuel.ehrick@gmail.com>
2025-07-07 16:28:46 -0400

    Added user prompt input via sys.argv[1]

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,14 +10,28 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
+    try:
+        user_input = sys.argv[1]
+        
+        messages = [
+        types.Content(role="user", parts=[types.Part(text=user_input)]),
+        ]
 
-    print(response.text)
+        response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=messages,
+        )
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(response.text)
+
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+        sys.exit(0)
+
+    except IndexError:
+        print("Error: No user input provided for prompt.")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
