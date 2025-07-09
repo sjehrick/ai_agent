e291ca384bac85afc99a1c79658c40ed3b12090c
arboreal <samuel.ehrick@gmail.com>
2025-07-08 22:39:21 -0400

    refactored to remove try/except logic. Added verbose flag functionality

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
-import os
 import sys
-from dotenv import load_dotenv
+import os
 from google import genai
 from google.genai import types
+from dotenv import load_dotenv
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
 
 def main():
-    try:
-        user_input = sys.argv[1]
-        
-        messages = [
-        types.Content(role="user", parts=[types.Part(text=user_input)]),
-        ]
+    load_dotenv()
 
-        response = client.models.generate_content(
-        model='gemini-2.0-flash-001',
-        contents=messages,
-        )
+    args = sys.argv[1]
+    
 
-        print(response.text)
-
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-        sys.exit(0)
-
-    except IndexError:
-        print("Error: No user input provided for prompt.")
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
+    user_prompt = args
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, user_prompt)
+
+def generate_content(client, messages, user_prompt):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    verbose = sys.argv[2:]
+    
+    print("Response:")
+    print(response.text)
+
+    if verbose and verbose == ['--verbose']:
+        print(f"User prompt: {user_prompt}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
 
 if __name__ == "__main__":
     main()
