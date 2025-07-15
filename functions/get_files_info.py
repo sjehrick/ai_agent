f01b54386c6503aadb975c2244d7779f67127785
arboreal <samuel.ehrick@gmail.com>
2025-07-15 17:29:59 -0400

    working though returning directory contents

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,7 +2,9 @@ import os
 
 def get_files_info(working_directory, directory=None):
     full_path = os.path.join(working_directory, directory)
-    if directory is not in os.path.abspath(working_directory):
+    if directory is not in working_directory:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(directory):
         return f'Error: "{directory}" is not a directory'
+    for item in os.listdir(directory):
+        
