b981c3ca1967f412da6a13f812ef81ea151b10aa
arboreal <samuel.ehrick@gmail.com>
2025-07-14 21:13:30 -0400

    added two if statements to get_files_info.py checking path and if parameter is a directory

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,2 +1,8 @@
+import os
+
 def get_files_info(working_directory, directory=None):
-    os.path.join(working_directory, directory)
+    full_path = os.path.join(working_directory, directory)
+    if directory is not in os.path.abspath(working_directory):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(directory):
+        return f'Error: "{directory}" is not a directory'
