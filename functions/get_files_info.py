c6736b940576d607ed1d7d0a7fee56769fdf7358
arboreal <samuel.ehrick@gmail.com>
2025-07-16 20:14:34 -0400

    wrote for loop to return data about objects in listed directory

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -7,4 +7,4 @@ def get_files_info(working_directory, directory=None):
     if not os.path.isdir(directory):
         return f'Error: "{directory}" is not a directory'
     for item in os.listdir(directory):
-        
+        return f'- {item}: file_size={os.path.getsize(item)} bytes, is_dir={os.path.isdir(item)}'
