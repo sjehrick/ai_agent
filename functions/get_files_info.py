aef1f670a8bb99650dba5b4c541b91f30138943f
arboreal <samuel.ehrick@gmail.com>
2025-07-19 18:19:10 -0400

    finished get files info func. Updated tests.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,10 +1,22 @@
 import os
 
-def get_files_info(working_directory, directory=None):
+def get_files_info(working_directory, directory="."):
     full_path = os.path.join(working_directory, directory)
-    if directory is not in working_directory:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(directory):
-        return f'Error: "{directory}" is not a directory'
-    for item in os.listdir(directory):
-        return f'- {item}: file_size={os.path.getsize(item)} bytes, is_dir={os.path.isdir(item)}'
+    abs_path_full_path = os.path.abspath(full_path)
+    abs_path_working = os.path.abspath(working_directory)
+    try:
+        if not abs_path_full_path.startswith(abs_path_working):
+            return f'   Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(full_path):
+            return f'   Error: "{directory}" is not a directory'
+        #if directory == ".":
+        output = ""
+        #else:
+        #    output = f"Result for '{directory}' directory:"
+        
+        for item in os.listdir(full_path):
+            output += f'\n- {item}: file_size={os.path.getsize(os.path.join(full_path, item))} bytes, is_dir={os.path.isdir(os.path.join(full_path, item))}'
+        return output
+    
+    except Exception as e:
+        return f'Error: An unexpected error occurred, {e} \nNeed you to try again!'
--- tests.py
+++ tests.py
@@ -1,40 +1,14 @@
-import unittest
 from functions.get_files_info import get_files_info
 
+print("Result for current directory:")
+print(get_files_info("calculator", "."))
 
-class TestCalculator(unittest.TestCase):
-    def setUp(self):
-        self.get_files_info = get_files_info()
+print("\nResult for 'pkg' directory:")
+print(get_files_info("calculator", "pkg"))
 
-    def test_current_dir(self):
-        result = self.get_files_info("calculator", ".")
-        self.assertEqual(result, 
-        "Result for current directory:
-        - main.py: file_size=576 bytes, is_dir=False
-        - tests.py: file_size=1343 bytes, is_dir=False
-        - pkg: file_size=92 bytes, is_dir=True
-        - lorem.txt: file_size=28 bytes, is_dir=False")
+print("\nResult for '/bin' directory:")
+print(get_files_info("calculator", "/bin"))
 
-    def test_pkg_dir(self):
-        result = self.get_files_info("calculator", "pkg")
-        self.assertEqual(result,
-        "Result for 'pkg' directory:
-        - calculator.py: file_size=1739 bytes, is_dir=False
-        - render.py: file_size=768 bytes, is_dir=False
-        - __pycache__: file_size=96 bytes, is_dir=True
-        - morelorem.txt: file_size=26 bytes, is_dir=False")
-     
-    def test_bin_dir(self):
-        result = self.get_files_info("calculator", "/bin")
-        self.assertEqual(result,
-        "Result for '/bin' directory:
-        Error: Cannot list "/bin" as it is outside the permitted working directory")
-    
-    def test_upalevel_dir(self):
-        result = self.get_files_info("calculator", "../")
-        self.assertEqual(result,
-        "Result for '../' directory:
-        Error: Cannot list "../" as it is outside the permitted working directory")
+print("\nResult for '../' directory:")
+print(get_files_info("calculator", "../"))
 
-if __name__ == "__main__":
-    unittest.main()
