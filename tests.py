495010e771857a4d70788fa76e7b9547b04d97da
arboreal <samuel.ehrick@gmail.com>
2025-07-18 22:12:27 -0400

    minor tweaks to test names

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.get_files_info = get_files_info()
 
-    def test_1(self):
+    def test_current_dir(self):
         result = self.get_files_info("calculator", ".")
         self.assertEqual(result, 
         "Result for current directory:
@@ -15,7 +15,7 @@ class TestCalculator(unittest.TestCase):
         - pkg: file_size=92 bytes, is_dir=True
         - lorem.txt: file_size=28 bytes, is_dir=False")
 
-    def test_2(self):
+    def test_pkg_dir(self):
         result = self.get_files_info("calculator", "pkg")
         self.assertEqual(result,
         "Result for 'pkg' directory:
@@ -24,13 +24,13 @@ class TestCalculator(unittest.TestCase):
         - __pycache__: file_size=96 bytes, is_dir=True
         - morelorem.txt: file_size=26 bytes, is_dir=False")
      
-    def test_3(self):
+    def test_bin_dir(self):
         result = self.get_files_info("calculator", "/bin")
         self.assertEqual(result,
         "Result for '/bin' directory:
         Error: Cannot list "/bin" as it is outside the permitted working directory")
     
-    def test_4(self):
+    def test_upalevel_dir(self):
         result = self.get_files_info("calculator", "../")
         self.assertEqual(result,
         "Result for '../' directory:
