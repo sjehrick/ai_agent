e9b3c91135312fcae17db261037ce48f21fe1208
arboreal <samuel.ehrick@gmail.com>
2025-07-21 21:18:01 -0400

    created logic for checking file path bounds and checking file path is a file

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,4 +1,11 @@
 import os
 
 def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.abspath(os.path.join(working_directory, file_path))
+    if not target_dir.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(target_dir):
+    return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
     
