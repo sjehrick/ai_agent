f2c6e02e7e464a06f28d1f42fa79fe9584a4345f
arboreal <samuel.ehrick@gmail.com>
2025-07-25 21:30:28 -0400

    fixed formatting

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -7,12 +7,15 @@ def get_file_content(working_directory, file_path):
     if not target_dir.startswith(abs_working_dir):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     if not os.path.isfile(target_dir):
-    return f'Error: File not found or is not a regular file: "{file_path}"'
+        return f'Error: File not found or is not a regular file: "{file_path}"'
     try:
         MAX_CHARS = 10000
         with open(target_dir, "r") as f:
             file_content_string = f.read(MAX_CHARS)
         if len(file_content_string) == MAX_CHARS:
-            file_content_string += f'...File "{target_dir}" truncated at 10000 characters'
+            file_content_string += (
+                f'[...File "{target_dir}" truncated at 10000 characters]'
+            )
+        return file_content_string
     except Exception as e:
-        return f'Error: error retrieving file content: {e}'
+        return f"Error: error retrieving file content: {e}"
