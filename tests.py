c9af606c5d3a1b897d03f9649ecee7726e4649c9
arboreal <samuel.ehrick@gmail.com>
2025-07-27 12:59:28 -0400

    updated tests.py with the correct tests

--- tests.py
+++ tests.py
@@ -1,10 +1,12 @@
 from functions.get_file_content import get_file_content
 
-def main():
-    class TestFileContent():
-        self.file_content = get_file_content(working_directory, file_path)
-        def _apply_operator(self, operators, values):
+print(get_file_content("calculator", "main.py"))
 
+print(get_file_content("calculator", "pkg/calculator.py"))
+
+print(get_file_content("calculator", "/bin/cat"))
+
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
         
 
 
