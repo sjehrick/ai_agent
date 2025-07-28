fccdf10b5afd3e270a075322b62a0117b9d14aa4
arboreal <samuel.ehrick@gmail.com>
2025-07-28 12:18:11 -0400

    created write_file.py with initial path checking logic

---
+++ functions/write_file.py
@@ -0,0 +1,7 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.abspath(os.path.join(working_directory, file_path))
+    if not target_dir.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
