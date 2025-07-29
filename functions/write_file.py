8c0ae7296517bc695dffd4e319a2290e80f54cff
arboreal <samuel.ehrick@gmail.com>
2025-07-29 11:33:13 -0400

    created initial try/except block for path checking and dir creation

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,7 +1,13 @@
 import os
 
+
 def write_file(working_directory, file_path, content):
     abs_working_dir = os.path.abspath(working_directory)
     target_dir = os.path.abspath(os.path.join(working_directory, file_path))
     if not target_dir.startswith(abs_working_dir):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    try:
+        if not os.path.exists(file_path):
+            os.makedirs(target_dir)
+    except Exception as e:
+        return f"Error: an unexpected error occured: {e}"
