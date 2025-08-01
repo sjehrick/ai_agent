98e734572d941948c448c6ccbf9900f23aa9265a
arboreal <samuel.ehrick@gmail.com>
2025-08-01 17:27:26 -0400

    updated tests.py for write_file.py

--- tests.py
+++ tests.py
@@ -1,12 +1,11 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
-print(get_file_content("calculator", "main.py"))
+print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
 
-print(get_file_content("calculator", "pkg/calculator.py"))
+print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
 
-print(get_file_content("calculator", "/bin/cat"))
+print(write_file("calculator", "/temp/temp.txt", "this should not be allowed"))
 
-print(get_file_content("calculator", "pkg/does_not_exist.py"))
         
 
 
