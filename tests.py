8c5f651ae15cfb0622447458982fa882efad537c
arboreal <samuel.ehrick@gmail.com>
2025-08-02 20:05:57 -0400

    formatted tests.py

--- tests.py
+++ tests.py
@@ -5,14 +5,3 @@ print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
 print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
 
 print(write_file("calculator", "/temp/temp.txt", "this should not be allowed"))
-
-        
-
-
-
-
-
-
-
-
-
