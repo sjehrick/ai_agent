450cd555d8cf541fb48277e284845486cc811f48
arboreal <samuel.ehrick@gmail.com>
2025-08-03 20:07:52 -0400

    fixed pathing for write_file

--- functions/write_file.py
+++ functions/write_file.py
@@ -4,12 +4,13 @@ import os
 def write_file(working_directory, file_path, content):
     abs_working_dir = os.path.abspath(working_directory)
     target_dir = os.path.abspath(os.path.join(working_directory, file_path))
+    file_path_dir = os.path.abspath(os.path.dirname(file_path))
     if not target_dir.startswith(abs_working_dir):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     try:
-        if not os.path.exists(file_path):
-            os.makedirs(target_dir)
-        with open(file_path, "w") as f:
+        if not os.path.exists(file_path_dir):
+            os.makedirs(file_path_dir)
+        with open(target_dir, "w") as f:
             f.write(content)
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
