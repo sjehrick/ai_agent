c2467f4ff1a3190e71096a5aca0dee13d92ee020
arboreal <samuel.ehrick@gmail.com>
2025-08-10 17:26:16 -0400

    crated separate prompts module for efficiency

--- functions/run_python.py
+++ functions/run_python.py
@@ -6,7 +6,7 @@ def run_python_file(working_directory, file_path, args=[]):
     abs_working_dir = os.path.abspath(working_directory)
     target_dir = os.path.abspath(os.path.join(working_directory, file_path))
     if not target_dir.startswith(abs_working_dir):
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
     if not os.path.exists(target_dir):
         return f'Error: File "{file_path}" not found.'
     if not file_path.endswith(".py"):
