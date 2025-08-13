f14b12574f0632d13383b534e8651018d5f3c5ab
arboreal <samuel.ehrick@gmail.com>
2025-08-13 10:09:29 -0400

    added basic printing for calling function with name and args if verbose set

--- functions/call_function.py
+++ functions/call_function.py
@@ -2,4 +2,8 @@ from google.genai import types
 
 
 def call_function(function_call_part, verbose=False):
+    if verbose:
+              print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    print(f" - Calling function: {function_call_part.name}")
+
 
