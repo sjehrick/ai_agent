7e989e3c69f57e8e9904b4723b29203f85511385
arboreal <samuel.ehrick@gmail.com>
2025-08-14 11:34:23 -0400

    created function dictionary to store callable functions

--- functions/call_function.py
+++ functions/call_function.py
@@ -3,7 +3,20 @@ from google.genai import types
 
 def call_function(function_call_part, verbose=False):
     if verbose:
-              print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
     print(f" - Calling function: {function_call_part.name}")
 
-
+    function_dict = {
+        "get_files_info": get_files_info(
+            **{"working_directory": "./calculator", "directory": "."}
+        ),
+        "get_file_content": get_file_content(
+            **{"working_directory": "./calculator", "file_path": "x"}
+        ),
+        "run_python": run_python_file(
+            **{"working_directory": "./calculator", "file_path": "x", "args": []}
+        ),
+        "write_file": write_file(
+            **{"working_directory": "./calculator", "file_path": "x", "content": "x"}
+        ),
+    }
